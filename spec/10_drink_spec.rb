0115b19d6c4a00460e3e4e8999d9850ebf90bcbc
Josh Cook <jcook2303@gmail.com>
2023-04-24 21:11:08 -0700

    Assignment 10 solutions. Using instead of class name for subject

--- spec/10_drink_spec.rb
+++ spec/10_drink_spec.rb
@@ -91,13 +91,15 @@ describe Drink do
     context 'when type is specified and ounces is default' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type.
-
+      subject(:default_drink) { described_class.new('tequila', 1.5) }
       # remove the 'x' before running this test
-      xit 'is your choice of beverage' do
+      it 'is your choice of beverage' do
+        expect(default_drink.type).to eq('tequila')
       end
 
       # remove the 'x' before running this test
-      xit 'has 16 ounces' do
+      it 'has 16 ounces' do
+        expect(default_drink.ounces).not_to eq(16)
       end
     end
   end
@@ -106,18 +108,20 @@ describe Drink do
     context 'when drink has 16 ounces or more' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type.
-
+      subject(:default_drink) { described_class.new('beer', 16) }
       # remove the 'x' before running this test
-      xit 'is full' do
+      it 'is full' do
+        expect(default_drink).to be_full
       end
     end
 
     context 'when drink has less than 16 ounces' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type. In addition, specify ounces to be any number under 16.
-
+      subject(:default_drink) { described_class.new('beer', 1) }
       # remove the 'x' before running this test
-      xit 'is not full' do
+      it 'is not full' do
+        expect(default_drink).not_to be_full
       end
     end
   end
